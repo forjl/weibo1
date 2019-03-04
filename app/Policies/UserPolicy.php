f645cbe64bb26060b8ff0944f154a7236aece86b
forjl <748409412@qq.com>
2019-03-04 06:05:33 +0000

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -18,4 +18,9 @@ class UserPolicy
     {
         return $curretUser->id === $user->id;
     }
+
+    public function destroy(User $currentUser,User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id;
+    }
 }
